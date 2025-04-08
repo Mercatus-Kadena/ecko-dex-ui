8acfc7d5eaf820e090b2bbe93bef812ab4672378
AmirDapl <Amir@daplcorp.com>
2025-04-08 10:31:46 -0500

    fix wrapper

--- src/constants/WrapperConfig.js
+++ src/constants/WrapperConfig.js
@@ -1,17 +1,11 @@
 const wrapperConfig = {
-    'coin:kaddex.kdx': { isBoosted: true },
-    'coin:n_b742b4e9c600892af545afb408326e82a6c0c6ed.zUSD': { isBoosted: true },
-    'coin:free.crankk01': { isBoosted: true }
-  };
-  
-  export const isWrapperBoosted = (token0, token1) => {
-    const pairKey = `${token0}:${token1}`;
-    const reversePairKey = `${token1}:${token0}`;
-    return (
-      (wrapperConfig[pairKey] && wrapperConfig[pairKey].isBoosted) ||
-      (wrapperConfig[reversePairKey] && wrapperConfig[reversePairKey].isBoosted) ||
-      false
-    );
-  };
-  
-  export default wrapperConfig;
+  'coin:kaddex.kdx': { },
+  'coin:n_b742b4e9c600892af545afb408326e82a6c0c6ed.zUSD': { },
+  'coin:free.crankk01': { }
+};
+
+export const isWrapperBoosted = (token0, token1) => {
+  return false;
+};
+
+export default wrapperConfig;
